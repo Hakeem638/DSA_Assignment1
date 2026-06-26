357aec318522ebb6c724e5ce53cb718f95000830
Hakeem638 <harunahakeem638@gmail.com>
2026-06-26 14:52:54 +0000

    done with todos 13 to 18 in the correctionsDeque class

--- Starter_Code/src/CorrectionDeque.java
+++ Starter_Code/src/CorrectionDeque.java
@@ -20,26 +20,40 @@ public class CorrectionDeque {
     }
 
     public boolean addRear(Request request) {
-        // TODO 15: Add a normal correction request to the rear using modulo arithmetic.
-        return false;
+        if (isFull()) return false;
+        rear = (rear + 1) % data.length;    // mpove rear forward, wrap if needed
+        data[rear] = request;       // insert at new rear
+        size++;
+        return true;
     }
 
     public boolean addFront(Request request) {
         if (isFull()) return false;
-        rear = (rear + 1) % data.length;  // move rear forward, wrap if needed
-        data[rear] = request;              // insert at new rear
+        front = (front - 1 + data.length) % data.length;  // step backward
+        data[front] = request;
         size++;
         return true;
     }
 
     public Request removeFront() {
-        // TODO 17: Remove and return the front request.
-        return null;
+        if (isEmpty()) return null;
+
+        Request item = data[front];   // save the item first
+        data[front] = null;  // clear the slot the item is
+        front = (front + 1) % data.length;  // then move front forward
+        size--;
+        return item;
     }
 
     public Request removeRear() {
         // TODO 18: Remove and return the rear request.
-        return null;
+        if (isEmpty()) return null;
+
+        Request item = data[rear];
+        data[rear] = null;
+        rear = (rear - 1 + data.length) % data.length;
+        size--;
+        return item; 
     }
 
     public int size() {
